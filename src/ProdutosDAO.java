41421a428c05a4eb368688f5b60ec286ec45c524
Eduardo Becker <dudu7.beck@gmail.com>
2023-06-26 21:15:15 -0300

    Ajustada conexão com banco de dados, inserido método para salvar produto no BD e telas de mensagem ao usuário referente ao casdastro de produto.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,42 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
-    Connection conn;
+
+    private conectaDAO conexao;
+    private Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public ProdutosDAO() {
+        this.conexao = new conectaDAO();
+        this.conn = this.conexao.connectDB();
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
+
+    public void cadastrarProduto(ProdutosDTO produto) {
         
+        // Inicia a transação
+        try {
+            conn.setAutoCommit(false);
+            
+            // Insere o produto no banco de dados
+            String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, produto.getNome());
+            stmt.setDouble(2, produto.getValor());
+            stmt.setString(3, produto.getStatus());
+            stmt.executeUpdate();
+            
+            // Confirma a transação
+            conn.commit();
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+        } catch (SQLException e) {
+            // Desfaz a transação em caso de erro
+            try {
+                conn.rollback();
+            } catch (SQLException rollbackEx) {
+                JOptionPane.showMessageDialog(null, "Erro ao desfazer transação: " + rollbackEx.getMessage());
+            }
+            JOptionPane.showMessageDialog(null, "Erro ao inserir produto: " + e.getMessage());
+        } finally {
+            // Restaura o modo de commit automático
+            try {
+                conn.setAutoCommit(true);
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Erro ao restaurar o modo de commit automático: " + e.getMessage());
+            }
+        }
+    }
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,33 +1,22 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
-    
-    public Connection connectDB(){
-        Connection conn = null;
-        
+
+    public Connection connectDB() {
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/uc11", // linha de conexao
+                    "Admin", // usuario do mysql
+                    "Rps@32admin"// senha do mysql
+            );
+            return conn;
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao conectar" + erro.getMessage());
+            return null;
         }
-        return conn;
     }
-    
 }
